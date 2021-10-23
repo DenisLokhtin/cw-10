60de0f12d27e3f44690294e047a520e78a4c8b78
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-10-23 20:22:50 +0600

    всё кроме удаления и добавления работает

---
+++ front/src/components/News/News.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const News = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+};
--- front/src/components/news/News.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const News = (props) => (
-    <div>News</div>
-);
-
-export default News;
